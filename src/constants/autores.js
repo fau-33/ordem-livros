5cfd5f345c7fc59d430ab32113ebcdeb7797c00a
Flávio Félix <fau_felix30@hotmail.com>
2024-11-29 16:13:55 -0300

    Conteudo dos autores

---
+++ src/constants/autores.js
@@ -0,0 +1,25 @@
+const autores = [
+  {
+    id: "j-k-rowling",
+    nome: "J.K. Rowling",
+    imagem:
+      "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/04/GettyImages-950041970-e1712065084538.jpg",
+    biografia: "J.K. Rowling (nascida em 31 de julho de 1965) é uma autora britânica famosa por criar a série Harry Potter, que se tornou um fenômeno mundial. A série é composta por sete livros que seguem as aventuras do jovem bruxo Harry Potter e seus amigos Hermione Granger e Ron Weasley.\n\nRowling enfrentou dificuldades financeiras antes do sucesso, escrevendo o primeiro livro em cafés enquanto cuidava de sua filha. Sua perseverança a levou a se tornar uma das autoras mais bem-sucedidas da história.\n\nAlém disso, Rowling é uma defensora ativa dos direitos humanos e fundadora da organização beneficente Lumos."
+  },
+  {
+    id: "j-r-r-tolkien",
+    nome: "J.R.R. Tolkien",
+    imagem:
+      "https://static.wixstatic.com/media/064821_b9fda45205e4495cac3a0d35ba514e86~mv2.jpeg/v1/fill/w_568,h_320,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/064821_b9fda45205e4495cac3a0d35ba514e86~mv2.jpeg",
+    biografia: "J.R.R. Tolkien (1892-1973) foi um escritor britânico amplamente reconhecido como o pai da literatura de fantasia moderna. Ele é mais conhecido por suas obras O Senhor dos Anéis e O Hobbit.\n\nTolkien começou sua carreira acadêmica como professor na Universidade de Oxford, onde lecionou literatura anglo-saxônica.\n\nPublicadas entre 1954-1955, suas obras estabeleceram novos padrões para a literatura fantástica."
+  },
+  {
+    id: "george-r-r-martin",
+    nome: "George R.R. Martin",
+    imagem:
+      "https://musicaecinema.com/wp-content/uploads/2024/11/george-rr-martin-spin-off-arya-stark-1024x576.webp",
+    biografia: "George R.R. Martin (nascido em 20 de setembro de 1948) é um autor americano conhecido pela série As Crônicas de Gelo e Fogo.\n\nEle começou sua carreira escrevendo contos na década de 1970 antes de se dedicar à literatura.\n\nA adaptação televisiva Game of Thrones se tornou um fenômeno cultural global."
+  },
+];
+
+export default autores;
