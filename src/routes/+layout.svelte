0bb26b045a5ecc4f88fa760f83f13a14e8c8c45c
Flávio Félix <fau_felix30@hotmail.com>
2024-11-28 22:13:35 -0300

    Feito o componente pagina

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import '../app.css';
-	import Logo from '../components/template/Logo.svelte';
+	import Pagina from '../components/template/Pagina.svelte';
 	let { children } = $props();
 </script>
 
-<div class="
-	flex flex-col justify-center items-center gap-5
-bg-black min-h-screen text-white
-">
 
-	<Logo />
-	{@render children()}
 
-</div>
+	<Pagina>
+		{@render children()}
+	</Pagina>
+
+		
+	
+
+
 
