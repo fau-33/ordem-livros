ca56c8604a4fa2cbd5a0ff46836debbeceddab86
Flávio Félix <fau_felix30@hotmail.com>
2024-11-29 17:16:05 -0300

    Componente Titulo

---
+++ src/components/shared/Titulo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let principal: string = "";
+  export let secundario: string = "";
+</script>
+
+<div class="flex flex-col">
+  <h1 class="text-3xl font-black">{principal}</h1>
+  <p class="text-sm text-zinc-400">{secundario}</p>
+</div>
--- src/routes/autores/+page.svelte
+++ src/routes/autores/+page.svelte
@@ -1,12 +1,18 @@
-<script>
+<script lang="ts">
   import ItemAutor from "../../components/autor/ItemAutor.svelte";
+  import Titulo from "../../components/shared/Titulo.svelte";
   import autores from "../../constants/autores";
 </script>
 
-<h1 class="text-3xl font-bold">Autores</h1>
+<div class="flex flex-col gap-10">
+  <Titulo
+    principal="Autores"
+    secundario="Conheça os autores que escrevem as histórias que você ama"
+  />
 
-<div class="grid grid-cols-2 gap-5">
-  {#each autores as autor}
-    <ItemAutor {autor} />
-  {/each}
+  <div class="grid grid-cols-2 gap-5">
+    {#each autores as autor}
+      <ItemAutor {autor} />
+    {/each}
+  </div>
 </div>
