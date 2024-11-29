5ff4930a2f988ba1cdea905d37685a102eeab74e
Flávio Félix <fau_felix30@hotmail.com>
2024-11-29 11:27:30 -0300

    Componente Rodapé

--- src/components/template/Pagina.svelte
+++ src/components/template/Pagina.svelte
@@ -1,13 +1,15 @@
 <script>
     import Cabecalho from "./Cabecalho.svelte";
+    import Rodape from "./Rodape.svelte";
     
 
 </script>
 <div class="flex flex-col min-h-screen bg-black text-white">
     <Cabecalho />
 
-    <main>
+    <main class="flex-1">
         <slot></slot>
     </main>
+    <Rodape />
 
 </div>
