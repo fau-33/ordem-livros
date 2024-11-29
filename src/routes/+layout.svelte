78e97d833421804a589dd45e0b9bae1b609a6df2
Flávio Félix <fau_felix30@hotmail.com>
2024-11-28 21:43:51 -0300

    continuando o projeto

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import Logo from '../components/template/Logo.svelte';
+	let { children } = $props();
+</script>
+
+<div class="
+	flex flex-col justify-center items-center gap-5
+bg-black min-h-screen text-white
+">
+
+	<Logo />
+	{@render children()}
+
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+
+  <h1 class=" text-3xl font-bold">Ordem dos livros</h1>
+  <a href="/autores">Autores</a>
+
+
+
+
